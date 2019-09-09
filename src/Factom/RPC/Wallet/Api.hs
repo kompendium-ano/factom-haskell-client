f72114a522c55f6a4f3a90b83d9629b018f84140
Sergey <sergey.bushnyak@sigrlami.eu>
2019-09-09 10:31:15 +0300

    add stubs for wallet and debug api

--- src/Factom/RPC/Wallet/Api.hs
+++ src/Factom/RPC/Wallet/Api.hs
@@ -1,1 +1,59 @@
 module Factom.RPC.Wallet.Api where
+
+import           Control.Concurrent
+import           Control.Exception                (bracket)
+import           Control.Monad.IO.Class
+import           Control.Remote.Monad.JSON
+import           Control.Remote.Monad.JSON.Client
+import           Control.Remote.Monad.JSON.Router
+import           Control.Remote.Monad.JSON.Trace
+import           Data.Aeson
+import           Data.Aeson.Types
+import           Data.Text
+import           Network.Socket                   (HostName, ServiceName,
+                                                   SocketType (Stream),
+                                                   addrAddress, addrFamily,
+                                                   addrProtocol, addrSocketType,
+                                                   close, connect, defaultHints,
+                                                   getAddrInfo, socket)
+
+import           Factom.RPC.JsonRpc               (JsonRpcT, runJsonRpcT)
+
+--------------------------------------------------------------------------------
+
+-- active-identity-keys
+-- add-ec-output
+-- add-fee
+-- add-input
+-- add-output
+-- address
+-- all-addresses
+-- all-identity-keys
+-- compose-chain
+-- compose-entry
+-- compose-identity-attribute
+-- compose-identity-attribute-endorsement
+-- compose-identity-chain
+-- compose-identity-key-replacement
+-- compose-transaction
+-- delete-transaction
+-- generate-ec-address
+-- generate-factoid-address
+-- generate-identity-key
+-- get-height
+-- identity-key
+-- import-addresses
+-- import-identity-keys
+-- import-koinify
+-- new-transaction
+-- properties
+-- remove-address
+-- remove-identity-key
+-- sign-data
+-- sign-transaction
+-- sub-fee
+-- tmp-transactions
+-- transactions (Retrieving)
+-- unlock-wallet
+-- wallet-backup
+-- wallet-balances
