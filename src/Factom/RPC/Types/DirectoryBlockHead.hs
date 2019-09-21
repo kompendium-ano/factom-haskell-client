bd0424fbf8dd28f85ae3286d84a44d74f83f62a4
Sergey <sergey.bushnyak@sigrlami.eu>
2019-09-21 12:22:15 +0300

    update directory block header

--- src/Factom/RPC/Types/DirectoryBlockHead.hs
+++ src/Factom/RPC/Types/DirectoryBlockHead.hs
@@ -1,81 +1,42 @@
-{-# LANGUAGE TemplateHaskell     #-}
-{-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE RecordWildCards     #-}
-{-# LANGUAGE OverloadedStrings   #-}
-{-# LANGUAGE TypeOperators       #-}
 {-# LANGUAGE DeriveGeneric       #-}
+{-# LANGUAGE OverloadedStrings   #-}
+{-# LANGUAGE RecordWildCards     #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TemplateHaskell     #-}
+{-# LANGUAGE TypeOperators       #-}
 
-module JsonDataDirectoryBlockHead where
+module Factom.RPC.Types.DirectoryBlockHead where
 
-import           System.Exit                    ( exitFailure
-                                                , exitSuccess
-                                                )
-import           System.IO                      ( stderr
-                                                , hPutStrLn
-                                                )
-import qualified Data.ByteString.Lazy.Char8    as BSL
-import           System.Environment             ( getArgs )
-import           Control.Monad                  ( forM_
-                                                , mzero
-                                                , join
-                                                )
 import           Control.Applicative
+import           Control.Monad                   (forM_, join, mzero)
+import           Data.Aeson                      (FromJSON (..), ToJSON (..),
+                                                  Value (..), decode, object,
+                                                  pairs, (.:), (.:?), (.=))
 import           Data.Aeson.AutoType.Alternative
-import           Data.Aeson                     ( decode
-                                                , Value(..)
-                                                , FromJSON(..)
-                                                , ToJSON(..)
-                                                , pairs
-                                                , (.:)
-                                                , (.:?)
-                                                , (.=)
-                                                , object
-                                                )
+import qualified Data.ByteString.Lazy.Char8      as BSL
 import           Data.Monoid
-import           Data.Text                      ( Text )
+import           Data.Text                       (Text)
 import qualified GHC.Generics
+import           System.Environment              (getArgs)
+import           System.Exit                     (exitFailure, exitSuccess)
+import           System.IO                       (hPutStrLn, stderr)
+
+--------------------------------------------------------------------------------
 
 -- | Workaround for https://github.com/bos/aeson/issues/287.
 o .:?? val = fmap join (o .:? val)
 
 
-data TopLevel = TopLevel {
+data DirectoryBlockHeader = DirectoryBlockHeader {
     topLevelKeymr :: Text
   } deriving (Show,Eq,GHC.Generics.Generic)
 
 
-instance FromJSON TopLevel where
-  parseJSON (Object v) = TopLevel <$> v .: "keymr"
+instance FromJSON DirectoryBlockHeader where
+  parseJSON (Object v) = DirectoryBlockHeader <$> v .: "keymr"
   parseJSON _          = mzero
 
 
-instance ToJSON TopLevel where
-  toJSON (TopLevel {..}) = object ["keymr" .= topLevelKeymr]
-  toEncoding (TopLevel {..}) = pairs ("keymr" .= topLevelKeymr)
-
-
-
-
-parse :: FilePath -> IO TopLevel
-parse filename = do
-  input <- BSL.readFile filename
-  case decode input of
-    Nothing -> fatal $ case (decode input :: Maybe Value) of
-      Nothing -> "Invalid JSON file: " ++ filename
-      Just v  -> "Mismatched JSON value from file: " ++ filename
-    Just r -> return (r :: TopLevel)
- where
-  fatal :: String -> IO a
-  fatal msg = do
-    hPutStrLn stderr msg
-    exitFailure
-
-main :: IO ()
-main = do
-  filenames <- getArgs
-  forM_
-    filenames
-    (\f -> parse f >>= (\p -> p `seq` putStrLn $ "Successfully parsed " ++ f))
-  exitSuccess
-
-
+instance ToJSON DirectoryBlockHeader where
+  toJSON (DirectoryBlockHeader {..}) = object ["keymr" .= topLevelKeymr]
+  toEncoding (DirectoryBlockHeader {..}) = pairs ("keymr" .= topLevelKeymr)
