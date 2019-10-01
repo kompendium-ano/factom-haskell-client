86b8cf0252f7556df9ffa1cea078e1d18bcf0e33
Sergey <sergey.bushnyak@sigrlami.eu>
2019-10-02 00:23:15 +0300

    update debug methods

--- src/Factom/RPC/Debug.hs
+++ src/Factom/RPC/Debug.hs
@@ -35,21 +35,119 @@ import           Factom.RPC.JsonRpc               (JsonRpcT, runJsonRpcT)
 endpoint  = "http://localhost:8088/debug"
 endpointRemote = "http://dev.factomd.net/v2/debug"
 
+-- |
+--
 reqHoldingQueue :: RPC ()
 reqHoldingQueue =
   method "holding-queue" None
 
+-- |
+--
 -- network-info
+reqNetworkInfo :: RPC ()
+reqNetworkInfo =
+  method "network-info" None
+
+
+-- |
+--
 -- predictive-fer
+reqPredictiveFer :: RPC ()
+reqPredictiveFer =
+  method "predictive-fer" None
+
+
+-- |
+--
 -- audit-servers
+reqAuditServers :: RPC ()
+reqAuditServers =
+  method "audit-servers" None
+
+
+-- |
+--
 -- federated-servers
+reqFederatedServers :: RPC ()
+reqFederatedServers =
+  method "federated-servers" None
+
+
+-- |
+--
 -- configuration
+reqConfiguration :: RPC ()
+reqConfiguration =
+  method "configuration" None
+
+
+-- |
+--
 -- process-list
+reqProccessList :: RPC ()
+reqProcessList =
+  method "process-list" None
+
+
+-- |
+--
 -- authorities
+reqAuthorities :: RPC ()
+reqAuthorities =
+  method "authorities" None
+
+
+-- |
+--
 -- reload-configuration
+reqReloadConfiguration :: RPC ()
+reqReloadConfiguration =
+  method "reload-configuration" None
+
+
+-- |
+--
 -- drop-rate
+reqDropRate :: RPC ()
+reqDropRate =
+  method "drop-rate" None
+
+
+-- |
+--
 -- set-drop-rate
+reqSetDropRate :: RPC ()
+reqSetDropRate =
+  method "set-drop-rate" None
+
+
+-- |
+--
 -- delay
+reqDelay :: RPC ()
+reqDelay =
+  method "delay" None
+
+
+-- |
+--
 -- set-delay
+reqSetDelay :: RPC ()
+reqSetDelay =
+  method "set-delay" None
+
+
+-- |
+--
 -- summary
+reqSummary :: RPC ()
+reqSummary =
+  method "summary" None
+
+
+-- |
+--
 -- messages
+reqMessages :: RPC ()
+reqMessages =
+  method "messages" None
