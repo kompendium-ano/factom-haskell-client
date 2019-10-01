b684ed59e03ead1a9fecfb183d1e859a37dfd711
Sergey <sergey.bushnyak@sigrlami.eu>
2019-10-01 22:23:15 +0300

    update wallet methods

--- README.md
+++ README.md
@@ -1,1 +1,60 @@
-# api-factom
+# Haskell Json RPC client for Factom blockchain
+
+[![Build Status](https://travis-ci.com/kompendium-llc/api-rpc-factom.svg?branch=master)](https://travis-ci.com/kompendium-llc/api-rpc-factom)
+[![License](https://img.shields.io/badge/license-MIT-blue.svg)](https://github.com/kompendium-llc/api-rpc-factom/blob/master/LICENSE)
+
+A JSON-RPC Haskell client for the Factom protocol. Each response has special ADT(algebraic data type) that automatically converted from JSON response.
+
+# Installation
+
+You can install package from [Hackage](https://hackage.haskell.org/package/api-rpc-factom) and build with Cabal, but we recommend to use [Stack](https://haskellstack.org) tool. Add to you dependencies in stack.yaml and cabal file dependency `- api-rpc-factom`.
+
+To run and test fromrepository
+
+1. Build with stack
+```bash
+$ stack build
+```
+
+2. Load REPL with stack for evaluation
+```
+$ stack repl
+```
+
+3. execute required methods
+
+## Usage
+
+for basic daemon functionality import with
+
+```haskell
+import Factom.RPC.Api
+```
+
+for wallet functionality
+
+
+```haskell
+import Factom.RPC.Wallet
+```
+
+#### Retreiving a balance
+
+```
+
+```
+
+#### Reading Entry Data
+```
+```
+#### Writing an Entry
+
+#### Block Height and Current Minute
+
+#### Sending A Transaction
+
+## Testing
+
+## Development
+
+To contribute to the library, clone the repository, create a feature branch and submit a PR to the develop branch.
--- src/Factom/RPC/Wallet/Api.hs
+++ src/Factom/RPC/Wallet/Api.hs
@@ -35,38 +35,181 @@ import           Factom.RPC.JsonRpc               (JsonRpcT, runJsonRpcT)
 endpoint = "http://localhost:8089/v2"
 
 -- active-identity-keys
+reqActiveIdentityKeys :: RPC ()
+reqActiveIdentityKeys =
+  method "active-identity-keys" None
+
 -- add-ec-output
+reqAddEcOutput :: RPC ()
+reqAddEcOutput =
+  method "add-ec-output" None
+
 -- add-fee
+reqAddFee :: RPC ()
+reqAddFee =
+  method "add-fee" None
+
 -- add-input
+reqAddInput :: RPC ()
+reqAddInput =
+  method "add-input" None
+
 -- add-output
+reqAddOutput :: RPC ()
+reqAddOutput =
+  method "add-output" None
+
 -- address
+reqAddress :: RPC ()
+reqAddress =
+  method "address" None
+
 -- all-addresses
+reqAllAddresses :: RPC ()
+reqAllAddresses =
+  method "all-addresses" None
+
 -- all-identity-keys
+reqAllIdentityKeys :: RPC ()
+reqAllIdentityKeys =
+  method "all-identity-keys" None
+
 -- compose-chain
+reqComposeChain :: RPC ()
+reqComposeChain =
+  method "compose-chain" None
+
 -- compose-entry
+reqComposeEntry :: RPC ()
+reqComposeEntry =
+  method "compose-entry" None
+
 -- compose-identity-attribute
+reqComposeIdentityAttribute :: RPC ()
+reqComposeIdentityAttribute =
+  method "compose-identity-attribute" None
+
 -- compose-identity-attribute-endorsement
+reqComposeIdentityAttributeEndorsement :: RPC ()
+reqComposeIdentityAttributeEndorsement =
+  method "compose-identity-attribute-endorsement" None
+
 -- compose-identity-chain
+reqComposeIdentityChain :: RPC ()
+reqComposeIdentityChain =
+  method "compose-identity-chain" None
+
 -- compose-identity-key-replacement
+reqComposeIdentityChainReplacement :: RPC ()
+reqComposeIdentityChainReplacement =
+  method "compose-identity-key-replacement" None
+
 -- compose-transaction
+reqComposeTransaction :: RPC ()
+reqComposeTransaction =
+  method "compose-transaction" None
+
 -- delete-transaction
+reqDeleteTransaction :: RPC ()
+reqDeleteTransaction =
+  method "delete-transaction" None
+
 -- generate-ec-address
+reqGenerateEcAddress :: RPC ()
+reqGenerateEcAddress =
+  method "generate-ec-address" None
+
 -- generate-factoid-address
+reqGenerateFactoidAddress :: RPC ()
+reqGenerateFactoidAddress =
+  method "generate-factoid-address" None
+
 -- generate-identity-key
+reqGenerateIdentityKey :: RPC ()
+reqGenerateIdentityKey =
+  method "generate-identity-key" None
+
 -- get-height
+reqGetHeight :: RPC ()
+reqGetHeight =
+  method "get-height" None
+
 -- identity-key
+reqIdentityKey :: RPC ()
+reqIdentityKey =
+  method "identity-key" None
+
 -- import-addresses
+reqImportAddresses :: RPC ()
+reqImportAddresses =
+  method "import-addresses" None
+
 -- import-identity-keys
+reqImportIdentityKeys :: RPC ()
+reqImportIdentityKeys =
+  method "import-identity-keys" None
+
 -- import-koinify
+reqImportKoinify :: RPC ()
+reqImportKoinify =
+  method "import-koinify" None
+
 -- new-transaction
+reqNewTransaction :: RPC ()
+reqNewTransaction =
+  method "new-transaction" None
+
 -- properties
+reqPropertiess :: RPC ()
+reqPropertiess =
+  method "properties" None
+
 -- remove-address
+reqRemoveAddress :: RPC ()
+reqRemoveAddress =
+  method "remove-address" None
+
 -- remove-identity-key
+reqRemoveIdentityKey :: RPC ()
+reqRemoveIdentityKey =
+  method "remove-identity-key" None
+
 -- sign-data
+reqSignData :: RPC ()
+reqSignData =
+  method "sign-data" None
+
 -- sign-transaction
+reqSignTransaction :: RPC ()
+reqSignTransaction =
+  method "sign-transaction" None
+
 -- sub-fee
+reqSubFee :: RPC ()
+reqSubFee =
+  method "sub-fee" None
+
 -- tmp-transactions
+reqTmpTransactions :: RPC ()
+reqTmpTransactions =
+  method "tmp-transactions" None
+
 -- transactions (Retrieving)
+reqRetrievingTransactions :: RPC ()
+reqRetrievingTransactions =
+  method "transactions (Retrieving)" None
+
 -- unlock-wallet
+reqUnlockWallet :: RPC ()
+reqUnlockWallet =
+  method "unlock-wallet" None
+
 -- wallet-backup
+reqWalletBackup :: RPC ()
+reqWalletBackup =
+  method "wallet-backup" None
+
 -- wallet-balances
+reqWalletBalances :: RPC ()
+reqWalletBalances =
+  method "wallet-balances" None
