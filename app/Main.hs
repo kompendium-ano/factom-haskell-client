87a73bb9079859f6223f8259da0dc9da568d1233
Sergey Bushnyak <sergey.bushnyak@sigrlami.eu>
2021-02-28 11:57:06 +0200

    version bump; update deps; update copyrights

--- app/Main.hs
+++ app/Main.hs
@@ -9,25 +9,24 @@ import qualified Data.Text                   as T
 import           Data.Time.Clock
 import           System.Environment
 
-import qualified Factom.RPC.Api              as FCTApi
-import qualified Factom.RPC.Types            as FCTApi
+import qualified Factom.RPC.Api              as FApi
+import qualified Factom.RPC.Types            as FApi
 
 --------------------------------------------------------------------------------
-
 main :: IO ()
-main =
-  do
-    let delay = 60000  -- 1 request per minute available
-    a       <- getArgs
-    timeVar <- newTVarIO (delay*1000)
-    case a of
-      ["-t"]    -> do
+main = do
+  let delay = 60000 -- 1 request per minute available
+  a <- getArgs
+  timeVar <- newTVarIO (delay * 1000)
+  case a of
+    ["-t"]
         -- read token from file
-        return ()
-      otherwise -> do
-        putStrLn $ "Factom | Establishing connection"
-        putStrLn $ "Factom | Getting last entries\n"
-        --mentries <- MBApi.getEntries
+     -> do
+      return ()
+    otherwise -> do
+      putStrLn $ "Factom | Establishing connection"
+      putStrLn $ "Factom | Getting last entries\n"
+        --mentries <- FApi.getEntries
         --putStrLn $ show $ mcurrs
-        -- mapM_ FCTApi.showEntryBlock mentries
-        return ()
+        -- mapM_ FApi.showEntryBlock mentries
+      return ()
